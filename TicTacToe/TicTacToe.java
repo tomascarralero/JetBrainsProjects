4442c5486cdbfb34f81eaa2af64fe7371612c7e1
tomascarralero <tcarralero@gmail.com>
2020-08-25 20:34:37 +0200

    Refactor of updateState method and minor changes

--- TicTacToe/TicTacToe.java
+++ TicTacToe/TicTacToe.java
@@ -1,20 +1,19 @@
 import java.util.Collections;
 import java.util.Scanner;
 
-public class TicTacToe {
+public class Main {
 
     private static final int ROWS = 3;
     private static final int COLS = 3;
     private static final char BLANK = '_';
     private static final String FILL_STRING = String.join("", Collections.nCopies(9, String.valueOf(BLANK)));
 
-
-    private static int valueX;
-    private static int valueY;
     private static char turn = 'X';
     private static Scanner sc = new Scanner(System.in);
     private static char[][] gameBoard = new char[ROWS][COLS];
     private static GameState state = GameState.PLAYING;
+    private static int xCounter = 0;
+    private static int oCounter = 0;
 
     public static void main(String[] args) {
         fillMatrix(gameBoard);
@@ -49,8 +48,8 @@ public class TicTacToe {
                 System.out.println("You should enter numbers!");
             }
 
-            valueX = sc.nextInt();
-            valueY = sc.nextInt();
+            int valueX = sc.nextInt();
+            int valueY = sc.nextInt();
 
             if(valueX < 1 || valueX > 3 || valueY < 1 || valueY > 3) {
                 System.out.println("Coordinates should be from 1 to 3!");
@@ -109,67 +108,29 @@ public class TicTacToe {
         }
     }
 
-    private static void updateState(char[][] arr) {
-
+    static void updateState(char[][] arr) {
         boolean xWins = false;
         boolean oWins = false;
-        boolean freeCells = false;
-        int hSum = 0;
-        int vSum = 0;
-        int dSum1 = 0;
-        int dSum2 = 0;
-        int xCounter = 0;
-        int oCounter = 0;
 
-        for (int i = 0; i < 3; i++) { // Horizontal and freeCells evaluation
-            hSum = (int) arr[i][0] + arr[i][1] +arr[i][2];
-            for (int j = 0; j < 3; j++) {
-                if (arr[i][j] == BLANK) {
-                    freeCells = true;
-                } else if (arr[i][j] == 'X') {
-                    xCounter++;
-                } else if (arr[i][j] == 'O') {
-                    oCounter++;
-                }
-            }
-            if (hSum == 264) {
-                xWins = true;
-                hSum = 0;
-            } else if (hSum == 237) {
-                oWins = true;
-                hSum = 0;
-            }
-        }
+        updateCounters(arr);
 
-        for (int i = 0; i < 3; i++) { // Vertical evaluation
-            for (int j = 0; j < 3; j++) {
-                vSum += (int) arr[j][i];
-            }
-            if (vSum == 264) {
-                xWins = true;
-                vSum = 0;
-            } else if (vSum == 237) {
-                oWins = true;
-                vSum = 0;
-            } else {
-                vSum = 0;
-            }
-        }
-
-        dSum1 += (int) arr[0][0] + arr[1][1] + arr[2][2]; // First Diagonal evaluation
-
-        dSum2 += (int) arr[0][2] + arr[1][1] + arr[2][0]; // Second Diagonal evaluation
-
-        if (dSum1 == 264 || dSum2 == 264) {
+        if (horizontalEvaluation(arr, 'X')
+                || verticalEvaluation(arr, 'X')
+                || diagonalEvaluation(arr, 'X')) {
             xWins = true;
-        } else if (dSum1 == 237 || dSum2 == 237) {
+        }
+
+        if (horizontalEvaluation(arr, 'O')
+                || verticalEvaluation(arr, 'O')
+                || diagonalEvaluation(arr, 'O')) {
             oWins = true;
         }
 
-        if (Math.abs(xCounter - oCounter) > 1 || xWins && oWins) {
-            state = GameState.ERROR;
-        } else if (freeCells && !xWins && !oWins) {
+        if (freeCellsEvaluation(arr) && !xWins && !oWins) {
             state = GameState.PLAYING;
+            resetCounters();
+        } else if (xWins && oWins || Math.abs(xCounter - oCounter) > 1) {
+            state = GameState.ERROR;
         } else if (xCounter + oCounter == 9 && !xWins && !oWins) {
             state = GameState.FINISHED;
         } else if (xWins) {
@@ -177,6 +138,73 @@ public class TicTacToe {
         } else if (oWins) {
             state = GameState.O_WINS;
         }
+    }
+
+    static boolean freeCellsEvaluation(char[][] arr) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (arr[i][j] == BLANK) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    static void updateCounters(char[][] arr) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (arr[i][j] == 'X') {
+                    xCounter++;
+                } else if (arr[i][j] == 'O') {
+                    oCounter++;
+                }
+            }
+        }
+    }
+
+    static void resetCounters() {
+        xCounter = 0;
+        oCounter = 0;
+    }
+
+    static boolean horizontalEvaluation(char[][] arr, char player) {
+        int hSum = 0;
+        for (int i = 0; i < 3; i++) {
+            hSum = (int) arr[i][0] + arr[i][1] +arr[i][2];
+            if (hSum == 264 && player == 'X' || hSum == 237 && player == 'O') {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static boolean verticalEvaluation(char[][] arr, char player) {
+        int vSum = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                vSum += (int) arr[j][i];
+            }
+            if (vSum == 264 && player == 'X' || vSum == 237 && player == 'O') {
+                return true;
+            }
+            vSum = 0;
+        }
+        return false;
+    }
+
+    static boolean diagonalEvaluation(char[][] arr, char player) {
+        int dSum1 = 0;
+        int dSum2 = 0;
+
+        dSum1 += (int) arr[0][0] + arr[1][1] + arr[2][2];
+        dSum2 += (int) arr[0][2] + arr[1][1] + arr[2][0];
+
+        if ((dSum1 == 264 || dSum2 == 264) && player == 'X'
+                || (dSum1 == 237 || dSum2 == 237) && player == 'O') {
+            return true;
+        }
+        return false;
     }
 
     enum GameState {
